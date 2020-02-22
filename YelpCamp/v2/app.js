bfaa55733d800511e09cbe23c56ebb08bfa7dc7f
RobZahn <rob.jzahn@gmail.com>
2020-02-22 03:31:23 -0500

    Connect to MongoDB

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const uri = require('./config/keys').mongoURI;
+
+mongoose
+	.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true })
+	.then(() => console.log('Connected to MongoDB'))
+	.catch((err) => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -8,17 +15,17 @@ app.set('view engine', 'ejs');
 const campgrounds = [
 	{
 		name: 'Salmon Creek',
-		image: 'https://pixabay.com/get/5fe8d143495ab108f5d084609620367d1c3ed9e04e507441772e7fdc9e4dc4_340.jpg'
+		image: 'https://pixabay.com/get/50e9d4474856b108f5d084609620367d1c3ed9e04e507441772c79d6954ac5_340.jpg'
 	},
 
 	{
 		name: 'Granite Hill',
-		image: 'https://pixabay.com/get/57e0d6424954ac14f6da8c7dda793f7f1636dfe2564c704c7d2d7ad09f44c559_340.jpg'
+		image: 'https://pixabay.com/get/54e5dc474355a914f6da8c7dda793f7f1636dfe2564c704c7d2d78d6954fc258_340.jpg'
 	},
 
 	{
 		name: "Mountain Goat's Rest",
-		image: 'https://pixabay.com/get/54e5dd424856ae14f6da8c7dda793f7f1636dfe2564c704c7d2d7ad09f44c559_340.jpg'
+		image: 'https://pixabay.com/get/57e8d0424a5bae14f6da8c7dda793f7f1636dfe2564c704c7d2d78d6954fc258_340.jpg'
 	}
 ];
 
