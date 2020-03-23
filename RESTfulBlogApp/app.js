be2a869b888e594222a294cee19141efc91b7bf8
RobZahn <rob.jzahn@gmail.com>
2020-03-23 04:50:00 -0400

    Add show route

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -30,6 +30,8 @@ app.get('/', function(req, res) {
 	res.redirect('/blogs');
 });
 
+//INDEX
+
 app.get('/blogs', function(req, res) {
 	Blog.find({}, function(err, blogs) {
 		if (err) {
@@ -38,6 +40,28 @@ app.get('/blogs', function(req, res) {
 			res.render('index', { blogs: blogs });
 		}
 	});
+});
+
+//NEW
+app.get('/blogs/new', function(req, res) {
+	res.render('new');
+});
+
+//CREATE
+app.post('/blogs', function(req, res) {
+	Blog.create(req.body.blog, function(err, newBlog) {
+		if (err) {
+			res.render('new');
+		} else {
+			res.redirect('/blogs');
+		}
+	});
+});
+
+//SHOW
+
+app.get('/blogs/:id', function(req, res) {
+	res.send('Show page');
 });
 
 app.listen(3000, function() {
